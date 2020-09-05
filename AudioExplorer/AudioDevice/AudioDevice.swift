f929d79e8bc19024181ec79b27f0f33b077fe159
Andrew Monshizadeh <amonshiz@gmail.com>
2020-09-05 14:15:15 -0400

    Add name computed property to AudioDevice

--- AudioExplorer/AudioDevice/AudioDevice.swift
+++ AudioExplorer/AudioDevice/AudioDevice.swift
@@ -7,7 +7,61 @@
 
 import Foundation
 import CoreAudio
+import AudioToolbox
 
 public struct AudioDevice {
   let id: AudioDeviceID
 }
+
+extension AudioDevice {
+  var name: String {
+    var namePropertyAddress = AudioObjectPropertyAddress(
+      mSelector: AudioObjectPropertySelector(kAudioDevicePropertyDeviceNameCFString),
+      mScope: AudioObjectPropertyScope(kAudioObjectPropertyScopeGlobal),
+      mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementWildcard))
+
+    var nameSize: UInt32 = 0
+    let nameSizeStatus = AudioObjectGetPropertyDataSize(
+      self.id,
+      &namePropertyAddress,
+      0, nil, /* NO IDEA */
+      &nameSize)
+
+    guard nameSizeStatus == kAudioServicesNoError else {
+      fatalError("unable to get size of name: \(nameSizeStatus)")
+    }
+
+    var cfStringSize = UInt32(MemoryLayout<CFString>.size)
+    var nameCFString: Unmanaged<CFString>?
+    let nameStatus = AudioObjectGetPropertyData(
+      self.id,
+      &namePropertyAddress,
+      0, nil, /* NO IDEA */
+      &cfStringSize,
+      &nameCFString)
+
+    guard nameStatus == kAudioServicesNoError else {
+      fatalError("unable to get name: \(nameStatus)")
+    }
+
+    return nameCFString?.takeRetainedValue() as String? ?? "UNKNOWN"
+  }
+}
+
+
+#if DEBUG
+import SwiftUI
+
+struct ADPreview: PreviewProvider {
+  static var previews: some View {
+    let devices = AudioDeviceManager.shared.allDevices
+
+    ForEach(devices.indices) { index in
+      VStack {
+        Text("\(devices[index].id)")
+        Text("\(devices[index].name)")
+      }
+    }
+  }
+}
+#endif
