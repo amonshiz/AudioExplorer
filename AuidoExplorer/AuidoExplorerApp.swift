b6166ac3e6bb8b8422b63704598e2962fdf45178
Andrew Monshizadeh <amonshiz@gmail.com>
2020-09-05 13:23:19 -0400

    Initial Commit

---
+++ AuidoExplorer/AuidoExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuidoExplorerApp.swift
+//  AuidoExplorer
+//
+//  Created by Andrew Monshizadeh on 9/5/20.
+//
+
+import SwiftUI
+
+@main
+struct AuidoExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
