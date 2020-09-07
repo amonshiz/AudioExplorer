3b1302df299b3cc1f164252959ce8e2ee59f5e93
Andrew Monshizadeh <amonshiz@gmail.com>
2020-09-07 13:14:59 -0400

    Formatting

--- AudioExplorer/ContentView.swift
+++ AudioExplorer/ContentView.swift
@@ -8,29 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      let devices = AudioDeviceManager.shared.allDevices
+  var body: some View {
+    let devices = AudioDeviceManager.shared.allDevices
 
-      ForEach(devices.indices) { index in
-        VStack {
-          Text("\(devices[index].id)")
-          Text("\(devices[index].name)")
-          if let image = devices[index].image {
-            Image(nsImage: image)
-          }
-          if devices[index].input {
-            Text("Has Input")
-          }
-          if devices[index].output {
-            Text("Has Output")
-          }
+    ForEach(devices.indices) { index in
+      VStack {
+        Text("\(devices[index].id)")
+        Text("\(devices[index].name)")
+        if let image = devices[index].image {
+          Image(nsImage: image)
+        }
+        if devices[index].input {
+          Text("Has Input")
+        }
+        if devices[index].output {
+          Text("Has Output")
         }
       }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
