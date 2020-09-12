bd26839f2a9da6fac7e2e727f93c5fd710a4b77f
Andrew Monshizadeh <amonshiz@gmail.com>
2020-09-12 14:28:22 -0400

    Factor out CoreAudio property functions to "provider" struct that can then be used for mocking in tests

--- AudioExplorer/AudioDevice/AudioDevicePropertyWrapper.swift
+++ AudioExplorer/AudioDevice/AudioDevicePropertyWrapper.swift
@@ -8,44 +8,62 @@
 import Foundation
 import CoreAudio
 
-@propertyWrapper struct DeviceProperty<Base: AnyObject, Output> {
-  var id: AudioObjectID = 0
-  private var description: AudioDevicePropertyDescription<Base>
+internal struct PropertyProviderFunctions {
+  let hasCheck: (AudioObjectID, UnsafePointer<AudioObjectPropertyAddress>) -> Bool
+  let dataSize: (AudioObjectID, UnsafePointer<AudioObjectPropertyAddress>, UInt32, UnsafeRawPointer?, UnsafeMutablePointer<UInt32>) -> OSStatus
+  let getData: (AudioObjectID, UnsafePointer<AudioObjectPropertyAddress>, UInt32, UnsafeRawPointer?, UnsafeMutablePointer<UInt32>, UnsafeMutableRawPointer) -> OSStatus
 
-  init(_ description: AudioDevicePropertyDescription<Base>) {
+  let noErrorValue: OSStatus
+
+  static let standard = PropertyProviderFunctions(
+    hasCheck: AudioObjectHasProperty,
+    dataSize: AudioObjectGetPropertyDataSize,
+    getData: AudioObjectGetPropertyData,
+    noErrorValue: kAudioHardwareNoError)
+}
+
+
+@propertyWrapper struct DeviceProperty<Base: AnyObject, Output> {
+
+  var id: AudioObjectID = 0
+  private let description: AudioDevicePropertyDescription<Base>
+  private let providers: PropertyProviderFunctions
+
+  init(_ description: AudioDevicePropertyDescription<Base>, providers functions: PropertyProviderFunctions = .standard) {
     self.description = description
+    self.providers = functions
   }
 
   var wrappedValue: Output? {
     get {
       var address = description.address
-      guard AudioObjectHasProperty(id, &address) else {
+      guard providers.hasCheck(id, &address) else {
         print("property for address not available \(address)")
         return nil
       }
 
       var propertySize: UInt32 = 0
-      let sizeStatus = AudioObjectGetPropertyDataSize(
+      let sizeStatus = providers.dataSize(
         id,
         &address,
         0, nil,
         &propertySize)
 
-      guard sizeStatus == kAudioHardwareNoError else {
+      guard sizeStatus == providers.noErrorValue else {
         print("unable to get size for address \(address)")
         return nil
       }
 
       var typeSize = description.elementSize
       var holder: Unmanaged<Base>?
-      let accessStatus = AudioObjectGetPropertyData(
+      let accessStatus = providers.getData(
         id,
         &address,
         0, nil,
         &typeSize,
         &holder)
 
-      guard accessStatus == kAudioHardwareNoError else {
+      guard accessStatus == providers.noErrorValue else {
         print("unable to access address \(address)")
         return nil
       }
