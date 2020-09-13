9bcfb695755caac046386d50b14e3294fa6ecd8a
Andrew Monshizadeh <amonshiz@gmail.com>
2020-09-13 14:41:13 -0400

    Disable unused tests

--- AudioExplorerTests/AudioExplorerTests.swift
+++ AudioExplorerTests/AudioExplorerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import AudioExplorer
 
 class AudioExplorerTests: XCTestCase {
-
+/*
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -29,5 +29,5 @@ class AudioExplorerTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+*/
 }
