e75adea6dd2984a56ee914dea50472ed8b2e93e7
Andrew Monshizadeh <amonshiz@gmail.com>
2020-09-26 13:52:25 -0400

    Add debugging parameter and move label to member variable on DevicePicker

--- AudioExplorer/ContentView.swift
+++ AudioExplorer/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
   var body: some View {
     let devices = AudioDeviceManager.shared.allDevices
 
-    DevicePicker(availableDevices: devices)
+    DevicePicker(availableDevices: devices, label: Text("All devices"))
   }
 }
 
--- AudioExplorer/Views/DevicePicker.swift
+++ AudioExplorer/Views/DevicePicker.swift
@@ -7,28 +7,36 @@
 
 import SwiftUI
 
-struct DevicePicker: View {
+struct DevicePicker<Label>: View where Label: View {
   @State var selected: AudioDevice.ID
   var selectedDevice: AudioDevice? {
     get {
       availableDevices.first { $0.id == selected }
     }
   }
-  let availableDevices: [AudioDevice]
 
-  init(availableDevices: [AudioDevice]) {
+  let availableDevices: [AudioDevice]
+  let label: Label
+  private let debugging: Bool
+
+  init(availableDevices: [AudioDevice], label: Label, debugging: Bool = false) {
     assert(availableDevices.count > 0)
+    self.debugging = debugging
+    self.label = label
     self.availableDevices = availableDevices
     self._selected = State(initialValue: self.availableDevices.first!.id)
   }
 
   var body: some View {
-    Picker(selection: $selected, label: Text("Device")) {
+    Picker(selection: $selected, label: label) {
       ForEach(availableDevices) {
         Text("\($0.name ?? "")").id($0)
       }
     }
-    Text("selected \(selectedDevice?.name ?? "")")
+    .pickerStyle(MenuPickerStyle())
+    if debugging {
+      Text("selected \(selectedDevice?.name ?? "")")
+    }
   }
 }
 
@@ -36,6 +44,6 @@ struct DevicePicker_Previews: PreviewProvider {
   static var previews: some View {
     DevicePicker(availableDevices: [
       AudioDevice(id: 1)
-    ])
+    ], label: Text("Preview"))
   }
 }
